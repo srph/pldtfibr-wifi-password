32df0b3ab7e837acec25b67ae1862de464b88fd2
srph <seraphipod@gmail.com>
2015-12-17 23:30:37 +0800

    Optimize validate.

--- src/validate.js
+++ src/validate.js
@@ -1,8 +1,15 @@
 var table = require('./table');
-var codes = Object.keys(table).join('|');
-var VALID_REGEX = new RegExp(codes);
+var codes = null;
+var VALID_REGEX = null;
 
 module.exports = function validate(entry) {
+  // Previously, this assignment automatically ran when required.
+  // Now, we'll just cache the values once ran.
+  if ( VALID_REGEX == null ) {
+    codes = Object.keys(table).join('|');
+    VALID_REGEX = new RegExp(codes);
+  }
+
   if ( entry.length !== 6 || !VALID_REGEX.test(entry) ) {
     throw new Error('Entry (' + entry + ') is invalid!');
   }
