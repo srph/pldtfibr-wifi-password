d813ea289c099029dbd76026e0ddf974ee25241d
srph <seraphipod@gmail.com>
2015-12-17 23:33:08 +0800

    Refactor variable.

--- src/validate.js
+++ src/validate.js
@@ -1,15 +1,16 @@
 var table = require('./table');
-var codes = null;
 var VALID_REGEX = null;
 
 module.exports = function validate(entry) {
   // Previously, this assignment automatically ran when required.
   // Now, we'll just cache the values once ran.
   if ( VALID_REGEX == null ) {
-    codes = Object.keys(table).join('|');
+    var codes = Object.keys(table).join('|');
     VALID_REGEX = new RegExp(codes);
   }
 
+  // 6 - The convention used by PLDT.
+  // e.g., PLDTHOMEFIBR_c3c4c9 (the `c3c4c9`)
   if ( entry.length !== 6 || !VALID_REGEX.test(entry) ) {
     throw new Error('Entry (' + entry + ') is invalid!');
   }
